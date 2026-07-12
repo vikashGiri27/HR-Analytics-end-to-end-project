1af225f853c3790cd90c1bcc1ed2a4a2ef41fc37
vikashGiri27 <vikashgiri70670@gmail.com>
2026-07-12 11:01:34 +0530

    Add employee data table setup

--- Database/HR_Database_Setup.sql
+++ Database/HR_Database_Setup.sql
@@ -1,3 +1,31 @@
+#Database----------------------------------------------------------------------------------------
 create database hr_analytics;
 use hr_analytics;
-create table 
+
+#--------------------------------------------------------------------------------------------------
+#Employee Table dataset								
+
+create table employees(Employee_ID varchar(20) primary key,First_Name varchar(100),
+Last_Name varchar(100),Gender varchar(20),Date_of_Birth date,Email	varchar(100) unique,
+Phone_no varchar(20),Address varchar(100),City varchar(50),State varchar(50),
+Country varchar(50),Hire_Date date,Department_ID varchar(20),Job_ID	varchar(20),
+Manager_ID varchar(20) null,Salary decimal(10,2),Bonus decimal(10,2),Employment_Status varchar(50),
+Employment_Type varchar(50),Education varchar(50),Experience_Years int);
+
+desc employees;
+
+load data infile 'C:/ProgramData/MySQL/MySQL Server 8.0/Uploads/Hr_datasetfiles/Employees.csv'
+into table employees
+fields terminated by ','
+enclosed by '"'
+lines terminated by '\n'
+ignore 1 rows
+(Employee_ID ,First_Name,Last_Name,Gender,Date_of_Birth,Email,
+Phone_no,Address,City,State,Country,Hire_Date,Department_ID,Job_ID,
+@Manager_ID,Salary,Bonus,Employment_Status,Employment_Type,Education,Experience_Years)
+set manager_id=nullif(@manager_id,'');
+
+select * from employees;
+
+#------------------------------------------------------------------------------------------------
+
