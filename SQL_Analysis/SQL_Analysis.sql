988ab33ad64ae94305cf107f296d0bd305e68058
vikashGiri27 <vikashgiri70670@gmail.com>
2026-07-21 11:11:32 +0530

    analyze the average salary based on education level

--- SQL_Analysis/SQL_Analysis.sql
+++ SQL_Analysis/SQL_Analysis.sql
@@ -13,3 +13,11 @@ from employees group by year order by year asc;
 #Q3. Find top 10 highest paid employees.
 select concat(first_name,' ',last_name) as employee_name,sum(salary) as salary
 from employees group by concat(first_name,' ',last_name) order by salary asc limit 10;
+
+#Q4. calculate the average salary by education level.
+select education,round(avg(salary)) as salary from employees
+group by education order by salary desc;
+
+#Q5. Find the Year wise number of terminated employees.alter
+
+#Q6. Find the number of employees in each county.
