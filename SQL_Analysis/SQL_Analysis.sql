52f79418a68d40fdbc7d47727e0ff3bc5119449f
vikashGiri27 <vikashgiri70670@gmail.com>
2026-07-21 11:05:57 +0530

    analyze top 10 highest paid employees

--- SQL_Analysis/SQL_Analysis.sql
+++ SQL_Analysis/SQL_Analysis.sql
@@ -11,4 +11,5 @@ select year(hire_date) as year,count(*) as emp_count
 from employees group by year order by year asc;
 
 #Q3. Find top 10 highest paid employees.
-select concat(first
+select concat(first_name,' ',last_name) as employee_name,sum(salary) as salary
+from employees group by concat(first_name,' ',last_name) order by salary asc limit 10;
