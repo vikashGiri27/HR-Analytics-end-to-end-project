2b64847b8c90f201a729023a69239f1831bcbe0f
vikashGiri27 <vikashgiri70670@gmail.com>
2026-07-21 11:01:06 +0530

    analyze total employees and yearly hiring trends

--- SQL_Analysis/SQL_Analysis.sql
+++ SQL_Analysis/SQL_Analysis.sql
@@ -1,1 +1,14 @@
 use hr_analytics;
+
+/*--------------------------------------------------------------------
+                           Employees Table
+--------------------------------------------------------------------*/
+#Q1. Find the total numbers of employees.
+select count(*) as emp_count from employees;
+
+#Q2. Find the numbers of employees hired each year.
+select year(hire_date) as year,count(*) as emp_count
+from employees group by year order by year asc;
+
+#Q3. Find top 10 highest paid employees.
+select concat(first
