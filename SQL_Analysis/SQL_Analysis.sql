fa1b7fdca0894e57243b0038cbba7a7bbb1aa0b2
vikashGiri27 <vikashgiri70670@gmail.com>
2026-07-21 11:18:56 +0530

    Analyze top 10 most experienced employees

--- SQL_Analysis/SQL_Analysis.sql
+++ SQL_Analysis/SQL_Analysis.sql
@@ -15,9 +15,11 @@ select concat(first_name,' ',last_name) as employee_name,sum(salary) as salary
 from employees group by concat(first_name,' ',last_name) order by salary asc limit 10;
 
 #Q4. calculate the average salary by education level.
-select education,round(avg(salary)) as salary from employees
+select education,round(avg(salary),2) as salary from employees
 group by education order by salary desc;
 
-#Q5. Find the Year wise number of terminated employees.alter
+#Q5. Find the top 10 most experienced employees.
+select concat(first_name,' ',last_name) as employee_name,experience_years
+from employees order by Experience_Years desc limit 10;
 
 #Q6. Find the number of employees in each county.
