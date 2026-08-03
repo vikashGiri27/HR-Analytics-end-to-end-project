3f46c30d7c4fce15473271f2370a27f3b1876e64
vikashGiri27 <vikashgiri70670@gmail.com>
2026-08-03 23:39:27 +0530

    Analyze employee salary higher then the avg salary of their department

--- Advance_SQL_Analysis/Advance_SQL_Analysis.sql
+++ Advance_SQL_Analysis/Advance_SQL_Analysis.sql
@@ -1,1 +1,5 @@
+#Q1. Identify employees whose salary is higher than the average salary of their respective department.
+select e.employee_id,concat(e.first_name,' ',e.last_name) as employee_name
+,e.salary from employees e where salary>(select avg(e1.salary)
+from employees e1 where e.Department_ID=e1.department_id);
 
